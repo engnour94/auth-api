79afda9f0f66fd43da3cd3b9e2e0be340fecff15
engnour94 <nour.abuelenein@hotmail.com>
2021-05-27 04:34:59 +0300

    done

--- __tests__/auth.router.test.js
+++ __tests__/auth.router.test.js
@@ -1,167 +1,107 @@
 'use strict';
 
-require('dotenv').config();
 process.env.SECRET = 'toes';
 
 const server = require('../servers/server.js').server;
-require('@code-fellows/supergoose');
-
-const supertest = require("supertest")
-const fakeServer = supertest(server);
-
-// const mockRequest = supergoose(server);
-// const request = supergoose(server);
-// // let user = {
-// //   username: 'nour',
-// //   password: 'admin',
-// //   role: 'admin',
-// // };
-
-// // describe('Authentication Routes', () => {
-// //   it('POST -----> (/signup) it s creates a new user and sends an object with the user and the token to the client', async() => {
-// //     let test = await mockRequest.post('/signup').send(user);
-
-// //     expect(test.body.user.username).toEqual('nour');
-// //     expect(test.body.token).toBeDefined();
-// //     expect(test.status).toEqual(201);
-// //   });
-
-// //   it('POST /signin with basic authentication headers logs in a user and sends an object with the user and the token to the client', async() => {
-
-// //     let test = await mockRequest.post('/signin').auth(user.username, user.password);
+const supergoose = require('@code-fellows/supergoose');
 
 
-// //     expect(test.body.user.role).toEqual('admin');
-// //     expect(test.status).toEqual(200);
-// //   });
+const mockRequest = supergoose(server);
 
-
-// //   it('GET /users with bearer token and permission to delete as an admin', async() => {
-// //     let testS = await mockRequest.post('/signin').auth(user.username, user.password);
-
-// //     let token = ` Bearer ${testS.body.token}`;
-
-// //     let test = await mockRequest.get('/users').set(`Authorization`, `${token}`);
-
-
-// //     expect(test.body[0]).toEqual('nour');
-// //     expect(test.status).toEqual(200);
-// //   });
-
-
-// //   it('GET /secret with bearer token', async() => {
-// //     let testS = await mockRequest.post('/signin').auth(user.username, user.password);
-
-// //     let token = `Bearer ${testS.body.token}`;
-
-// //     let test = await mockRequest.get('/secret').set(`Authorization`, `${token}`);
-
-
-// //     expect(test.text).toEqual('Welcome to the secret area');
-// //     expect(test.status).toEqual(200);
-// //   });
-// // });
-
-
-let clothes = {
-  name: 'Shirt',
-  color: 'BLUE',
-  size: 'X-large',
+let users = {
+  admin: { username: 'admin', password: 'password', role: 'admin' },
+  editor: { username: 'editor', password: 'password', role: 'editor' },
+  user: { username: 'user', password: 'password', role: 'user' },
 };
-let user = {
-  username: 'fakeUser',
-  password: '0000',
-  role: 'admin',
-};
-
-describe('V2', () => {
-  it('POST /api/v2/:model with a bearer token that has create permissions adds an item to the DB and returns an object with the added item', async() => {
-
-    let siginUp = await fakeServer.post('/signup').send(user);
-    let siginIn = await fakeServer.post('/signin').auth(user.username, user.password);
-
-    let token = ` Bearer ${siginIn.body.token}`;
-
-
-    let test = await fakeServer.post('/api/v2/clothes').set(`Authorization`, token).send(clothes);
-
-
-    expect(test.body.color).toEqual('BLUE');
-    expect(test.status).toEqual(201);
+  
+describe('Auth Router', () => {
+  
+  Object.keys(users).forEach(userType => {
+  
+    describe(`${userType} users`, () => {
+  
+      it('can create one', async () => {
+  
+        const response = await mockRequest.post('/signup').send(users[userType]);
+        const userObject = response.body;
+  
+        expect(response.status).toBe(201);
+        expect(userObject.token).toBeDefined();
+        expect(userObject.user._id).toBeDefined();
+        expect(userObject.user.username).toEqual(users[userType].username);
+  
+      });
+  
+      it('can signin with basic', async () => {
+  
+        const response = await mockRequest.post('/signin')
+          .auth(users[userType].username, users[userType].password);
+  
+        const userObject = response.body;
+        expect(response.status).toBe(200);
+        expect(userObject.token).toBeDefined();
+        expect(userObject.user._id).toBeDefined();
+        expect(userObject.user.username).toEqual(users[userType].username);
+  
+      });
+  
+      it('can signin with bearer', async () => {
+        // First, use basic to login to get a token
+        const response = await mockRequest
+          .post('/signin')
+          .auth(users[userType].username, users[userType].password);
+  
+        const token = response.body.token;
+  
+        // First, use basic to login to get a token
+        const bearerResponse = await mockRequest
+          .get('/secret')
+          .set('Authorization', `Bearer ${token}`);
+  
+        // Not checking the value of the response, only that we "got in"
+        expect(bearerResponse.status).toBe(200);
+      });
+  
+    });
+  
+    describe('bad logins', () => {
+      it('basic fails with known user and wrong password ', async () => {
+  
+        const response = await mockRequest.post('/signin')
+          .auth('admin', 'xyz');
+        const userObject = response.body;
+  
+        expect(response.status).toBe(403);
+        expect(userObject.user).not.toBeDefined();
+        expect(userObject.token).not.toBeDefined();
+  
+      });
+  
+      it('basic fails with unknown user', async () => {
+  
+        const response = await mockRequest.post('/signin')
+          .auth('nobody', 'xyz');
+        const userObject = response.body;
+  
+        expect(response.status).toBe(403);
+        expect(userObject.user).not.toBeDefined();
+        expect(userObject.token).not.toBeDefined();
+  
+      });
+  
+      it('bearer fails with an invalid token', async () => {
+  
+        // First, use basic to login to get a token
+        const bearerResponse = await mockRequest
+          .get('/users')
+          .set('Authorization', `Bearer foobar`);
+  
+        // Not checking the value of the response, only that we "got in"
+        expect(bearerResponse.status).toBe(500);
+  
+      });
+    });
+  
   });
-
-
-  it('GET /api/v2/:model with a bearer token that has read permissions returns a list of :model items', async() => {
-
-    let siginIn = await fakeServer.post('/signin').auth(user.username, user.password);
-
-    let token = `Bearer ${siginIn.body.token}`;
-    let test = await fakeServer.get('/api/v2/clothes').set(`Authorization`, token);
-
-
-    expect(test.body[0].name).toEqual('Shirt');
-    expect(test.status).toEqual(200);
-  });
-
-
-  it('GET /api/v2/:model/ID with a bearer token that has read permissions returns a single item by ID', async() => {
-
-    let siginIn = await fakeServer.post('/signin').auth(user.username, user.password);
-
-    let token = `Bearer ${siginIn.body.token}`;
-
-    let postSomething = await fakeServer.post('/api/v2/clothes').set(`Authorization`, token).send(clothes);
-
-    let id = postSomething.body._id;
-
-    let testGEtById = await fakeServer.get(`/api/v2/clothes/${id}`).set(`Authorization`, token);
-
-    expect(testGEtById.status).toEqual(200);
-    expect(testGEtById.text).toBeDefined();
-  });
-
-
-  it('PUT /api/v2/:model/ID with a bearer token that has update permissions returns a single, updated item by ID', async() => {
-
-    let siginIn = await fakeServer.post('/signin').auth(user.username, user.password);
-
-    let token = `Bearer ${siginIn.body.token}`;
-
-    let postSomething = await fakeServer.post('/api/v2/clothes').set(`Authorization`, token).send(clothes);
-    let id = postSomething.body._id;
-
-    let updated = {
-      name: 'Shirt',
-      color: 'BLUE',
-      size: 'Small',
-    };
-
-    let updateSomething = await fakeServer.put(`/api/v2/clothes/${id}`).set(`Authorization`, token).send(updated);
-
-
-    expect(updateSomething.status).toEqual(200);
-    expect(updateSomething.body.size).toEqual('Small');
-  });
-
-
-  it('DELETE /api/v2/:model/ID with a bearer token that has delete permissions returns an empty object. Subsequent GET for the same ID should result in nothing found', async() => {
-
-    let siginIn = await fakeServer.post('/signin').auth(user.username, user.password);
-
-    let token = `Bearer ${siginIn.body.token}`;
-
-    let postSomething = await fakeServer.post('/api/v2/clothes').set(`Authorization`, token).send(clothes);
-    let id = postSomething.body._id;
-
-
-
-    let Todelete = await fakeServer.delete(`/api/v2/clothes/${id}`).set(`Authorization`, token);
-    let testGEtById = await fakeServer.get(`/api/v2/clothes/${id}`).set(`Authorization`, token);
-
-
-    expect(Todelete.status).toEqual(200);
-    expect(Todelete.data).not.toBeDefined();
-    expect(testGEtById.body).toBeNull();
-
-  });
+  
 });
